efbd67449bc0ecc7a3607e33c979ac2e42651ea9
Luca <luca.cavalcanti@studenti.unitn.it>
2023-12-20 16:17:20 +0100

    Add TODOs, Add K-Means, Add ideas for second portion of secondary compression criteria

--- src/clustering/BFR/bfr.py
+++ src/clustering/BFR/bfr.py
@@ -15,7 +15,6 @@ MAHALANOBIS_DISTANCE_THRESHOLD = 30
 Clusters = []       # the primary clusters
 RetainedSet = []    # the array of routes that did not fit any cluster
 CompressedSets = [] # the secondary clusters created with the Miniclusters and older CompressedSets
-Miniclusters = []   # the clusters created with the RetainedSet
 Buffer = []
 number_of_clusters = 0
 actual_routes_stream = None
@@ -76,6 +75,7 @@ def BFR(standard_routes, actual_routes):
         print()
     # do something with the final results
 
+# Set initial cluster centroids to the standard routes
 def init_clusters(standard_routes):
     global number_of_clusters
     number_of_clusters = 0
@@ -84,6 +84,7 @@ def init_clusters(standard_routes):
             Clusters.append(Cluster(route, number_of_clusters))
             number_of_clusters += 1
 
+# Open actual routes JSON file
 def init_actual_routes_stream(actual_routes):
     global actual_routes_stream
     actual_routes_stream = open(actual_routes, "rb")
@@ -99,7 +100,7 @@ def keep_filling_buffer():
             Buffer.append(route)
             if len(Buffer) == BUFFER_SIZE:
 
-                # do something with the buffer
+                # Buffer has been read completely, use it
                 stream_buffer()
                 Buffer.clear()
         
@@ -111,6 +112,11 @@ def keep_filling_buffer():
     except ijson.common.IncompleteJSONError:
         print("IncompleteJSONError")
 
+# For each route in the buffer, check:
+# - primary compression criteria: can we add the route directly in a cluster? (This is done by looking at the Mahalanobis distance)
+# - secondary compression criteria: all routes that are not added to clusters go to the RetainedSet. Cluster it with K-Means and then 
+#                                   compare these clusters with the CompressedSet. If needed and if possible, cluster the two sets 
+#                                   using Hierarchical clustering
 def stream_buffer():
     global Buffer, Clusters, RetainedSet
     primary_compression_criteria()
@@ -119,18 +125,21 @@ def stream_buffer():
 def primary_compression_criteria():
     global Buffer, Clusters, RetainedSet
     for route in Buffer:
+        
+        # Find closest cluster to route by passing all of them 
         closest_cluster, closest_distance = find_closest_cluster(route)    
+        # If closest cluster is under a certain distance threshold:
         if closest_distance < MAHALANOBIS_DISTANCE_THRESHOLD:
             # add the route to the cluster
             print("Adding route", route["id"], "to cluster", closest_cluster)
             Clusters[closest_cluster].add(route)
-            # remove the route from the buffer
-            Buffer = [r for r in Buffer if r != route]
         else:
             # add the route to the retained set
             print("Adding route", route["id"], "to retained set")
             RetainedSet.append(route)
-            Buffer = [r for r in Buffer if r != route]
+
+        # remove the route from the buffer
+        Buffer = [r for r in Buffer if r != route]
 
 def find_closest_cluster(route):
     closest_cluster = None
@@ -148,15 +157,24 @@ def mahalanobis_distance(route, centroid):
     # TODO: how do we calculate the distance between a route and a centroid?
     return 50
 
+# Cluster with K-Means the RetainedSet. We set k to be 30% more than the actual number of clusters in the Clusters[] array (the paper says to do so)
 def secondary_compression_criteria():
     global RetainedSet, CompressedSets
     k = int(number_of_clusters * 0.3 + number_of_clusters)
-    if len(RetainedSet) >= k:
+
+    # idea: we need at least two routes per cluster to have some kind of results
+    min_number_of_routes_to_cluster = 2*k
+    if len(RetainedSet) >= min_number_of_routes_to_cluster:
         # cluster the retained set with K-Means
         print("Clustering retained set with K-Means")
-        cluster_retained_set()
+        retainedSet_cluster_labels = cluster_retained_set(k)
+        # add the new clusters to the set of compressed sets
+        add_miniclusters_to_compressed_sets(retainedSet_cluster_labels)
 
-def cluster_retained_set():
+        # TODO: if the compressed sets are large enough, merge them with Hierarchical clustering
+
+def cluster_retained_set(k):
+    # In order
     global RetainedSet, CompressedSets
     distance_matrix = np.zeros((len(RetainedSet), len(RetainedSet)))
     city_indexes, cities_res, merch_indexes, merch_res = fe.get_features_total(RetainedSet)
@@ -166,11 +184,35 @@ def cluster_retained_set():
             distance_matrix[i][j] = custom_distance(cities_res[i], cities_res[j], merch_res[i], merch_res[j])
     # make the distance matrix symmetric
     distance_matrix = distance_matrix + distance_matrix.T
-    
+
+    # Perform K-Means clustering with the custom distance metric
+    kmeans = KMeans(n_clusters=k, init='k-means++', algorithm='full')
+    kmeans.fit(distance_matrix)
+
+    # Get cluster labels -> [0, 1, 1, 0] means route 0 has cluster index 0, route 1 has cluster index 1, etc.
+    labels = kmeans.labels_
+    print("Cluster labels:", labels)
+
+    # TODO: create miniclusters from the labels
+
+    return labels
 
 def custom_distance(cities_A, cities_B, merch_A, merch_B):
     # this function should, given the vectorial representation of routes A and B, parse their distance and return its value.
     return 1
+
+def add_miniclusters_to_compressed_sets(labels):
+    global RetainedSet, CompressedSets
+    # Count how many routes are in each cluster
+    clusters_count = {}
+    for i in range(len(labels)):
+        if labels[i] in clusters_count:
+            clusters_count[labels[i]] += 1
+        else:
+            clusters_count[labels[i]] = 1
+    
+    # TODO: Add the clusters to the Compressed Sets if they have at least 2 routes
+
 
 
 if __name__ == "__main__":
