f43d5a649a83908d488b31f913f23922d315e46c
Luca <luca.cavalcanti@studenti.unitn.it>
2023-12-25 23:10:40 +0100

    Add complete skeleton for BFR. WARNING: Placeholder values are still present, modify them accordingly

--- src/clustering/BFR/bfr.py
+++ src/clustering/BFR/bfr.py
@@ -8,15 +8,21 @@ import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances
 
+from sklearn.cluster import AgglomerativeClustering
+
+from copy import deepcopy
+
 # WARNING: these are placeholder values
 BUFFER_SIZE = 10
 MAHALANOBIS_DISTANCE_THRESHOLD = 30
 
 Clusters = []       # the primary clusters
 RetainedSet = []    # the array of routes that did not fit any cluster
+Miniclusters = []   # the clusters created with the RetainedSet
 CompressedSets = [] # the secondary clusters created with the Miniclusters and older CompressedSets
 Buffer = []
 number_of_clusters = 0
+number_of_compressed_sets = 0
 actual_routes_stream = None
 
 class Cluster:
@@ -28,15 +34,16 @@ class Cluster:
         # servono?
         self.sum = []
         self.sum_squares = []
+        self.routes = []
     
     def __str__(self):
         return f"Cluster: {self.index}\nCentroid: {self.centroid}\nSize: {self.size}"
 
     def add(self, route):
         self.size += 1
-        self.update_centroid(route)
+        self.routes.append(route)
     
-    def update_centroid(self, route):
+    def update_centroid(self):
         # TODO: how do we update a cluster's centroid?
         pass
 
@@ -72,6 +79,10 @@ def BFR(standard_routes, actual_routes):
     print("Retained Set:")
     for route in RetainedSet:
         print(route)
+        print()
+    print("Compressed Sets:")
+    for compressedset in CompressedSets:
+        print(compressedset)
         print()
     # do something with the final results
 
@@ -121,6 +132,9 @@ def stream_buffer():
     global Buffer, Clusters, RetainedSet
     primary_compression_criteria()
     secondary_compression_criteria()
+    # TODO: Update Clusters centroids
+    for cluster in Clusters:
+        cluster.update_centroid()
 
 def primary_compression_criteria():
     global Buffer, Clusters, RetainedSet
@@ -160,7 +174,8 @@ def mahalanobis_distance(route, centroid):
 # Cluster with K-Means the RetainedSet. We set k to be 30% more than the actual number of clusters in the Clusters[] array (the paper says to do so)
 def secondary_compression_criteria():
     global RetainedSet, CompressedSets
-    k = int(number_of_clusters * 0.3 + number_of_clusters)
+    # k = int(number_of_clusters * 0.3 + number_of_clusters)
+    k = 10 #TODO: REMOVE THIS PLAECHOLDER
 
     # idea: we need at least two routes per cluster to have some kind of results
     min_number_of_routes_to_cluster = 2*k
@@ -171,10 +186,25 @@ def secondary_compression_criteria():
         # add the new clusters to the set of compressed sets
         add_miniclusters_to_compressed_sets(retainedSet_cluster_labels)
 
-        # TODO: if the compressed sets are large enough, merge them with Hierarchical clustering
+    for cluster in CompressedSets:
+        cluster.update_centroid()
 
+    # TODO: if the compressed sets are large enough, merge them with Hierarchical clustering
+
+    # min_number_of_routes_to_cluster_compressedsets = 2 * number_of_clusters
+    min_number_of_routes_to_cluster_compressedsets = 5 #TODO: REMOVE THIS PLAECHOLDER
+
+    if number_of_compressed_sets >= min_number_of_routes_to_cluster_compressedsets:
+        # cluster the compressed sets with Hierarchical clustering
+        print("Clustering compressed sets with Hierarchical clustering")
+
+        # cluster_compressed_sets(number_of_clusters)
+        cluster_compressed_sets(5) #TODO: REMOVE THIS PLAECHOLDER
+
+    for cluster in CompressedSets:
+        cluster.update_centroid()
+    
 def cluster_retained_set(k):
-    # In order
     global RetainedSet, CompressedSets
     distance_matrix = np.zeros((len(RetainedSet), len(RetainedSet)))
     city_indexes, cities_res, merch_indexes, merch_res = fe.get_features_total(RetainedSet)
@@ -186,14 +216,12 @@ def cluster_retained_set(k):
     distance_matrix = distance_matrix + distance_matrix.T
 
     # Perform K-Means clustering with the custom distance metric
-    kmeans = KMeans(n_clusters=k, init='k-means++', algorithm='full')
+    kmeans = KMeans(n_clusters=k, init='k-means++', algorithm='lloyd', n_init=10)
     kmeans.fit(distance_matrix)
 
     # Get cluster labels -> [0, 1, 1, 0] means route 0 has cluster index 0, route 1 has cluster index 1, etc.
     labels = kmeans.labels_
     print("Cluster labels:", labels)
-
-    # TODO: create miniclusters from the labels
 
     return labels
 
@@ -202,18 +230,81 @@ def custom_distance(cities_A, cities_B, merch_A, merch_B):
     return 1
 
 def add_miniclusters_to_compressed_sets(labels):
-    global RetainedSet, CompressedSets
+    global RetainedSet, CompressedSets, number_of_compressed_sets
+
     # Count how many routes are in each cluster
     clusters_count = {}
-    for i in range(len(labels)):
+    labels_len = len(labels)
+    for i in range(labels_len):
         if labels[i] in clusters_count:
+            # K-Means cluster label was already considered, add route to respective CompressedSet and remove it from the RetainedSet
             clusters_count[labels[i]] += 1
         else:
+            # K-Means cluster label wasn't already considered, create new cluster to add to CompressedSet (can't remove route from RetainedSet, cluster might have size 1)
             clusters_count[labels[i]] = 1
+
+    new_retainedSet = []
+
+    indexes = {}
+    for i in range(labels_len):
+        if clusters_count[labels[i]] > 1:
+            if labels[i] in indexes:
+                CompressedSets[indexes[labels[i]]].add(RetainedSet[i])
+            else:
+                indexes[labels[i]] = number_of_compressed_sets
+                CompressedSets.append(Cluster(RetainedSet[i], number_of_compressed_sets))
+                number_of_compressed_sets += 1
+            print("Adding route", RetainedSet[i]["id"], "to compressed set", indexes[labels[i]])
+        else:
+            new_retainedSet.append(RetainedSet[i])
+            print("Adding route", RetainedSet[i]["id"], "to new retained set")
     
-    # TODO: Add the clusters to the Compressed Sets if they have at least 2 routes
+    RetainedSet = new_retainedSet
+            
+def cluster_compressed_sets(k):
+    global CompressedSets
+    distance_matrix = np.zeros((len(CompressedSets), len(CompressedSets)))
 
+    compressedSets_centroids = []
+    for cluster in CompressedSets:
+        compressedSets_centroids.append(cluster.centroid)
 
+    city_indexes, cities_res, merch_indexes, merch_res = fe.get_features_total(compressedSets_centroids)
+    for i in range(len(CompressedSets)):
+        # pick all routes from the next one to the end
+        for j in range(i + 1, len(CompressedSets)):
+            distance_matrix[i][j] = custom_distance(cities_res[i], cities_res[j], merch_res[i], merch_res[j])
+    # make the distance matrix symmetric
+    distance_matrix = distance_matrix + distance_matrix.T
+
+    # Perform hierarchical clustering
+    clustering = AgglomerativeClustering(n_clusters=k, metric='precomputed', linkage='average')
+    cluster_labels = clustering.fit_predict(distance_matrix)
+
+    update_CompressedSets(cluster_labels)
+
+def update_CompressedSets(labels):
+    global CompressedSets, number_of_compressed_sets
+
+    new_number_of_compressed_sets = 0
+
+    new_compressedSets = []
+
+    labels_len = len(labels)
+    indexes = {}
+    for i in range(labels_len):
+        if labels[i] in indexes:
+            new_compressedSets[indexes[labels[i]]].add(CompressedSets[i].centroid)
+            new_compressedSets[indexes[labels[i]]].size += CompressedSets[i].size - 1
+        else:
+            indexes[labels[i]] = new_number_of_compressed_sets
+            new_compressedSets.append(Cluster(CompressedSets[i].centroid, new_number_of_compressed_sets))
+            new_compressedSets[indexes[labels[i]]].size += CompressedSets[i].size - 1
+            new_number_of_compressed_sets += 1
+        print("Adding old cluster centroid", CompressedSets[i].centroid["id"], "to compressed set", indexes[labels[i]])
+    
+    number_of_compressed_sets = new_number_of_compressed_sets
+    CompressedSets = new_compressedSets
 
 if __name__ == "__main__":
     BFR("data/small/standard_small.json", "data/small/actual_normal_small.json")
