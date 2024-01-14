fb90081f230ab3665b5970acea3f759679c54efb
GaiaPizzutiUniTn <gaia.pizzuti@studenti.unitn.it>
2024-01-14 16:28:42 +0100

    Add time to find_perfect_route.py

--- src/comparing/find_perfect_route.py
+++ src/comparing/find_perfect_route.py
@@ -26,9 +26,14 @@ dict<string, dict>
 import json
 import random
 import sys
+from time import time
 sys.path.append('src/clustering/similarities')
 from similarity import similarity
 from feature_extractions import get_features
+
+
+def get_elapsed_time(start: float, end: float):
+    return round(end - start, 15)
 
 
 def get_route(actual_file: str):
@@ -91,6 +96,7 @@ def find_perfect_route(driver_id: int, actual_routes: dict):
     
     total_length = 0
     partial_length = 0
+    time_data = 0
     for actual in actuals:
         cities = dict()
         merchandise = dict()
@@ -109,13 +115,20 @@ def find_perfect_route(driver_id: int, actual_routes: dict):
                     merchandise[city][merch] += trip['merchandise'][merch]
                 else:
                     merchandise[city][merch] = trip['merchandise'][merch]
+        start_data_analyzation = time()
         analize_merch(merchandise, cities, data_merchandise)
         sum_mean_cities(total_cities, cities, partial_length)
+        end_data_analyzation = time()
+        time_data += (get_elapsed_time(start_data_analyzation, end_data_analyzation))
         partial_length = 0
     
+    print('time to analyze data given by the actuals', time_data)
     new_route_length = int(total_length / len(actuals))
     
+    start_sorting = time()
     sorted_cities = dict(sorted(total_cities.items(), key=lambda item: item[1], reverse=True))
+    end_sorting = time()
+    print('time to sort the dictionary containing the cities information:', get_elapsed_time(start_sorting, end_sorting))
     
     # take the n cities in order to create the new route where n is the length calculate in new_route_length
     cities_to_insert = list()
@@ -198,7 +211,7 @@ def find_next_city(sorted_cities: dict, sorted_index: int):
         if index == sorted_index + 1:
             return element, sorted_cities[element]
         index += 1
- """
+"""
 '''
 milano milano milano milano milano genova genova genova genova genova firenze
 
@@ -338,9 +351,14 @@ def find_perfect_route_per_driver(actuals: dict, result_file: str, city_weight =
     output = open(result_file, 'w')
     output.write('[\n')
     index = 0
+    mean_time = list()
     drivers = get_drivers(actuals)
     for driver in drivers:
+        start_time = time()
         route = find_perfect_route(driver, actuals)
+        end_time = time()
+        mean_time.append(get_elapsed_time(start_time, end_time))
+        print('time to find the perfect route for', driver, ':', end_time - start_time)
         driver_actuals = get_actual_to_driver(driver, actuals)
         compare_perfect_actuals(driver_actuals, route, city_weight, merch_weight)
         json_output = json.dumps(route)
@@ -351,6 +369,8 @@ def find_perfect_route_per_driver(actuals: dict, result_file: str, city_weight =
             output.write(',\n')
         index += 1
     output.write(']')
+    print('mean time to find the perfect route:', sum(mean_time) / len(mean_time))
+    
     
 def compare_perfect_actuals(actuals, perfect, city_weight: int, merch_weight: int, is_perfect: bool = True):
     '''
@@ -373,7 +393,7 @@ def compare_perfect_actuals(actuals, perfect, city_weight: int, merch_weight: in
             print('len perfect', len(perfect['route']))
             print()
     
-    print('sum of similarities for', perfect['driver'], ':', sum)
+    print('sum of similarities for', perfect['driver'], ':', sum, '\n')
         
 def test_perfects(actuals, city_weight = 0.7196538657216474, merch_weight = 0.28034613427835264):
     index = random.randint(0, len(actuals))
